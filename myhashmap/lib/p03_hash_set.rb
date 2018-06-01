2a6d834499117323951a5ee923ce7b907813d3c4
Matt <aaron.hardowar@stonybrook.edu>
2018-06-01 17:02:18 -0400

    Killed linked list

--- myhashmap/lib/p03_hash_set.rb
+++ myhashmap/lib/p03_hash_set.rb
@@ -7,18 +7,36 @@ class HashSet
   end
 
   def insert(key)
+    return if self.include?(key)
+    
+    resize! if @count == num_buckets 
+    
+    hashed = key.hash 
+    modded_hash = hashed % num_buckets
+    @store[modded_hash] << key 
+    @count += 1 
   end
 
   def include?(key)
+    hashed = key.hash 
+    modded_hash = hashed % num_buckets 
+    @store[modded_hash].include?(key)
   end
 
   def remove(key)
+    return unless self.include?(key)
+    
+    hashed = key.hash 
+    modded_hash = hashed % num_buckets 
+    @store[modded_hash].delete(key)
+    @count -= 1 
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    row, col = num 
+    self[row][col]
   end
 
   def num_buckets
@@ -26,5 +44,15 @@ class HashSet
   end
 
   def resize!
+    new_buckets = (num_buckets * 2)
+    new_arr = Array.new(new_buckets) { Array.new }
+    
+    @store.flatten.each do |key|
+      hashed_key = key.hash 
+      modded_key = hashed_key % new_buckets
+      new_arr[modded_key] << key 
+    end 
+    @store = new_arr
+      
   end
 end
--- myhashmap/lib/p04_linked_list.rb
+++ myhashmap/lib/p04_linked_list.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+ 
 class Node
   attr_accessor :key, :val, :next, :prev
 
@@ -11,15 +13,29 @@ class Node
   def to_s
     "#{@key}: #{@val}"
   end
+  
+  def add(node) 
+    node.prev = self 
+    node.next = self.next 
+    self.next.prev = node 
+    self.next = node 
+  end 
 
   def remove
-    # optional but useful, connects previous link to next link
-    # and removes self from list.
+    @prev.next = @next 
+    @next.prev = @prev 
   end
 end
 
 class LinkedList
+  
+  include Enumerable
+  
   def initialize
+    @head = Node.new('head') 
+    @tail = Node.new('tail') 
+    @head.next = @tail
+    @tail.prev = @head  
   end
 
   def [](i)
@@ -28,34 +44,71 @@ class LinkedList
   end
 
   def first
+    @head.next 
   end
 
   def last
+    @tail.prev 
   end
 
   def empty?
+    return true if @head.next == @tail 
+    false 
   end
 
   def get(key)
+    return nil if empty?
+    
+    self.each do |node|
+      return node.val if node.key == key 
+    end 
   end
 
-  def include?(key)
+  def include?(key, &prc)
+    return false if empty?
+    
+    self.each do |node|
+      return true if node.key == key 
+    end
+    false  
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    @tail.prev.add(new_node) 
   end
 
   def update(key, val)
+    return if empty? 
+
+    self.each do |node|
+      if node.key == key 
+        node.val = val
+        return  
+      end 
+    end 
   end
 
   def remove(key)
+    raise "List is empty" if empty?
+    
+    self.each do |node|
+      if node.key == key 
+        node.remove
+        return 
+      end 
+    end 
   end
 
-  def each
+  def each(&prc) 
+    node = first 
+    until node == @tail 
+      prc.call(node)
+      node = node.next 
+    end 
   end
 
-  # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
