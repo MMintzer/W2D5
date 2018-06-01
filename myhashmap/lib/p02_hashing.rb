5d09829f2e9c8309f809636efc4ad9789055a49c
Matt <aaron.hardowar@stonybrook.edu>
2018-06-01 15:08:53 -0400

    crushed p02_hashing.rb

--- myhashmap/lib/p02_hashing.rb
+++ myhashmap/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    return self.length.hash if self.length <= 1 
+    self.inject(:-).hash 
   end
 end
 
 class String
   def hash
+    alphabet = ("a".."z").to_a
+    num = (0..9).to_a 
+    alphabet.concat(num)
+    self.chars.map { |ch| alphabet.index(ch) }.join.to_i.hash
   end
 end
 
@@ -16,6 +22,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.length.hash
   end
 end
